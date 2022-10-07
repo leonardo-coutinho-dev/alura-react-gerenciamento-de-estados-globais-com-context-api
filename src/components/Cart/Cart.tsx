9982c0aa9ede762f38a40599a01b44460b16222b
leonardo <web.dev.trabalho@gmail.com>
2022-10-07 15:15:52 -0300

    adding navigation buttons to screens

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,22 +1,42 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import React from "react";
 
 const Cart = () => {
   return (
-    <Box
-      sx={{
-        maxWidth: "796px",
-        margin: "16px auto",
-        padding: "16px",
-        display: "flex",
-        flexDirection: "column",
-        backgroundColor: "#ebebeb",
-        color: "#1d1d1d",
-        borderRadius: "4px",
-      }}
-    >
-      Cart
-    </Box>
+    <>
+      <Box
+        sx={{
+          maxWidth: "796px",
+          margin: "16px auto",
+          padding: "16px",
+          display: "flex",
+          flexDirection: "column",
+          backgroundColor: "#ebebeb",
+          color: "#1d1d1d",
+          borderRadius: "4px",
+        }}
+      >
+        Cart
+      </Box>
+      <Box
+        sx={{
+          maxWidth: "796px",
+          margin: "0 auto 16px",
+          padding: "16px",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "flex-start",
+          backgroundColor: "#ebebeb",
+          color: "#1d1d1d",
+          borderRadius: "4px",
+        }}
+      >
+        <Button sx={{ maxWidth: "200px", backgroundColor: "#1d1d1d" }}>
+          {"<"} Anterior
+        </Button>
+      </Box>
+    </>
   );
 };
 
--- src/components/Shop/Shop.tsx
+++ src/components/Shop/Shop.tsx
@@ -1,22 +1,45 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import React from "react";
 
 const Shop = () => {
   return (
-    <Box
-      sx={{
-        maxWidth: "796px",
-        margin: "16px auto",
-        padding: "16px",
-        display: "flex",
-        flexDirection: "column",
-        backgroundColor: "#ebebeb",
-        color: "#1d1d1d",
-        borderRadius: "4px",
-      }}
-    >
-      Shop
-    </Box>
+    <>
+      <Box
+        sx={{
+          maxWidth: "796px",
+          margin: "16px auto",
+          padding: "16px",
+          display: "flex",
+          flexDirection: "column",
+          backgroundColor: "#ebebeb",
+          color: "#1d1d1d",
+          borderRadius: "4px",
+        }}
+      >
+        Shop
+      </Box>
+      <Box
+        sx={{
+          maxWidth: "796px",
+          margin: "0 auto 16px",
+          padding: "16px",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          backgroundColor: "#ebebeb",
+          color: "#1d1d1d",
+          borderRadius: "4px",
+        }}
+      >
+        <Button sx={{ maxWidth: "200px", backgroundColor: "#1d1d1d" }}>
+          {"<"} Anterior
+        </Button>
+        <Button sx={{ maxWidth: "200px", backgroundColor: "#1d1d1d" }}>
+          Próxima {">"}
+        </Button>
+      </Box>
+    </>
   );
 };
 
